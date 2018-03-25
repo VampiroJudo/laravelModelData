6c9b70046fc4f3e09bc3433c44ab1c868650f4e8
VampiroJudo <johnconley507@hotmail.com>
2018-03-25 15:49:27 -0400

    changing methods used for updating information

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,19 +10,19 @@ use Illuminate\Session\Store;
 
 class PostController extends Controller
 {
-    public function getIndex(Store $session)
+    public function getIndex()
     {
         $posts = Post::all();
         return view('blog.index', ['posts' => $posts]);
     }
 
-    public function getAdminIndex(Store $session)
+    public function getAdminIndex()
     {
         $post = Post::all();
         return view('admin.index', ['posts' => $posts]);
     }
 
-    public function getPost(Store $session, $id)
+    public function getPost($id)
     {
         $post = Post::find($id);
         return view('blog.post', ['post' => $post]);
@@ -33,13 +33,13 @@ class PostController extends Controller
         return view('admin.create');
     }
 
-    public function getAdminEdit(Store $session, $id)
+    public function getAdminEdit($id)
     {
-        $post = Post::find($id);
+        $post = Post::find();
         return view('admin.edit', ['post' => $post, 'postId' => $id]);
     }
 
-    public function postAdminCreate(Store $session, Request $request)
+    public function postAdminCreate(Request $request)
     {
         $this->validate($request, [
             'title' => 'required|min:5',
@@ -59,8 +59,10 @@ class PostController extends Controller
             'title' => 'required|min:5',
             'content' => 'required|min:10'
         ]);
-        $post = new Post();
-        $post->editPost($session, $request->input('id'), $request->input('title'), $request->input('content'));
+        $post = Post::find($request->input('id'));
+        $post->title= $request->input('title');
+        $post->content = $request->('content');
+        $post->save();
         return redirect()->route('admin.index')->with('info', 'Post edited, new Title is: ' . $request->input('title'));
     }
 }
